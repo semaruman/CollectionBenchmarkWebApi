18a376df4a7c0fbe12e8be1fd805df260fa0f93c
Semen <semen.ruman@gmail.com>
2026-03-30 16:28:01 +0300

    Добавьте файлы проекта.

---
+++ CollectionBenchmarkWebApi/Controllers/BenchmarkController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CollectionBenchmarkWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BenchmarkController : ControllerBase
+    {
+        [HttpGet("types")]
+        public IActionResult Types()
+        {
+            return Ok(new
+            {
+                Types = new[]
+                {
+                    "List", "Dictionary", "HashSet", "LinkedList", "SortedSet"
+                }
+            });
+        }
+    }
+}
