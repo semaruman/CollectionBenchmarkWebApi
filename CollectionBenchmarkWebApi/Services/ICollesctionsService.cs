206fa73643538cc6644b7282a6262d3923656d00
Semen <semen.ruman@gmail.com>
2026-03-30 17:27:27 +0300

    Добавил ICollectionService для отслеживания типов коллекций для сравнения и колличество элементов

---
+++ CollectionBenchmarkWebApi/Services/ICollesctionsService.cs
@@ -0,0 +1,9 @@
+﻿namespace CollectionBenchmarkWebApi.Services
+{
+    public interface ICollesctionsService
+    {
+        public List<string> Types { get; set; }
+
+        public int ElementsCount { get; set; }
+    }
+}
